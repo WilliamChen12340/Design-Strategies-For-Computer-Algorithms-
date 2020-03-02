3554fb813fbf195ebff1f264cc9540c9e8325d33
WilliamChen12340 <31947682+WilliamChen12340@users.noreply.github.com>
2020-03-02 23:16:18 +0800

    second commit

--- 2DCP.cpp
+++ 2DCP.cpp
@@ -1,114 +1,115 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-vector<pair<int, int>> ans_pair;
-bool cmp(pair<int, int> p1, pair<int, int> p2){
-	return p1.second < p2.second;
+bool mycmp(const pair<pair<int,int>,size_t> &point1, const pair<pair<int,int>,size_t> &point2)
+{
+	return point1.first.second < point2.first.second;
 }
-/*
-int _2DCP(vector<pair<int, int>> v){
-	//initial condition
-	
+long long _2DCP(vector<pair<pair<int,int>,size_t>> point, vector<pair<size_t,size_t>> &ans)
+{	size_t N = point.size();
+	//initial
+	if(point.size() == 1)
+		return numeric_limits<long long>::max();
+	if(point.size() == 2)
+	{
+		ans.push_back({min(point.begin()->second, (point.begin()+1)->second), max(point.begin()->second, (point.begin()+1)->second)});
+		return pow((point.begin()->first).first - ((point.begin()+1)->first).first,2)\
+			   + pow((point.begin()->first).second - ((point.begin()+1)->first).second,2);
+	}
 	//divide into L and R
-	vector<pair<int, int>> tmp = v;
-	nth_element(tmp.begin(), tmp.begin()+int((N-1)/2), tmp.end());
-	int xmedian = (tmp.begin()+int((N-1)/2))->first;
-	int cnt = 0;
-	for(auto i : v)
+	auto tmp = point;
+	nth_element(tmp.begin(), tmp.begin()+(N-1)/2, tmp.end());
+	auto median = *(tmp.begin()+(N-1)/2);
+	decltype(point) Lpoint, Rpoint;
+	for(auto i : point)
 	{
-		if(i.first == xmedian)
-			cnt++;
-	}
-	vector<pair<int, int>> vL, vR;
-	//vL.resize(int((N+1)/2));
-	//vR.resize(int(N/2));
-	int j = 0;
-	cout<<(tmp.begin()+int((N-1)/2))->first<<endl;
-	for(auto i : v) 
-	{
-		if(i < (*(tmp.begin()+int((N-1)/2))))
-			vL.push_back(i);
-		else if(i > (*(tmp.begin()+int((N-1)/2))))
-			vR.push_back(i);
+		if(i <= median)
+			Lpoint.push_back(i);
 		else
-		{
-			if(j <= (cnt+1)/2)
-			{
-				vL.push_back(i);
-				j++;
-			}
-			else
-				vR.push_back(i);
-		}
+			Rpoint.push_back(i);
 	}
+	// cout<<"median "<<median.second<<" ("<<median.first.first<<", "<<median.first.second<<")"<<endl;
+	// for(auto i : Lpoint)
+	// 	cout<<"Lpoint "<<i.second<<" ("<<i.first.first<<", "<<i.first.second<<")"<<endl;
+	// cout<<"----------------"<<endl;
+	// for(auto i : Rpoint)
+	// 	cout<<"Rpoint "<<i.second<<" ("<<i.first.first<<", "<<i.first.second<<")"<<endl;
+	// cout<<"################"<<endl;
 	//recursive
-	int dL = _2DCP(vL);
-	int dR = _2DCP(vR);
-	int d = min(dL, dR);
+	vector<pair<size_t,size_t>> Lans, Rans;
+	long long d;
+	long long dL = _2DCP(Lpoint, Lans);
+	long long dR = _2DCP(Rpoint, Rans);
+	if(dL < dR)
+	{
+		d = dL;
+		ans = Lans;
+	}
+	else if(dL > dR)
+	{
+		d = dR;
+		ans = Rans;
+	}
+	else
+	{
+		d = dL;
+		ans = Lans;
+		ans.insert(ans.end(), Rans.begin(), Rans.end());
+	}
 	//combine
-	vector<pair<int, int>> sL, sR;
-	auto it_L = vL.end()-1;
-	while(pow((vL.end()-1)->first - it_L->first),2 <= d)
+	double D = sqrt((double)d);
+	decltype(point) sL, sR;
+	for(auto i : Lpoint)
 	{
-		sL.push_back(*it_L);
-		it_L++;
+		if(i.first.first >= median.first.first - D)
+			sL.push_back(i);
 	}
-	auto it_R = vR.begin();
-	while(pow((vL.end()-1)->first - it_R->first),2 <= d)
+	for(auto i : Rpoint)
 	{
-		sR.push_back(*it_R);
-		it_R++;
+		if(i.first.first <= median.first.first + D)
+			sR.push_back(i);	
 	}
-
-}
-*/
-int main(){
-	int N;
-	cin >> N;
-	vector<pair<int, int>> v;
-	v.resize(N);
-	for(int i=0; i < N; i++)
-		cin >> v.at(i).first >> v.at(i).second;
-	sort(v.begin(), v.end(), cmp);
-
-
-
-	vector<pair<int, int>> tmp = v;
-	nth_element(tmp.begin(), tmp.begin()+int((N-1)/2), tmp.end());
-	int xmedian = (tmp.begin()+int((N-1)/2))->first;
-	int cnt = 0;
-	for(auto i : v)
+	auto Lowest_itR = sR.begin();
+	for(auto i : sL)
 	{
-		if(i.first == xmedian)
-			cnt++;
-	}
-	vector<pair<int, int>> vL, vR;
-	int j = 0;
-	for(auto i : v) 
-	{
-		if(i < (*(tmp.begin()+int((N-1)/2))))
-			vL.push_back(i);
-		else if(i > (*(tmp.begin()+int((N-1)/2))))
-			vR.push_back(i);
-		else
+		while(Lowest_itR != sR.end() && (Lowest_itR->first).second < i.first.second - D)
+			Lowest_itR++;
+		if(Lowest_itR == sR.end())	break;
+		auto itR = Lowest_itR;
+		while(itR != sR.end() && (itR->first).second <= i.first.second + D && (itR->first).second >= i.first.second - D)
 		{
-			if(j <= (cnt+1)/2)
+			if(pow((itR->first).first - i.first.first,2) + pow((itR->first).second - i.first.second,2) == d)
 			{
-				vL.push_back(i);
-				j++;
+				ans.push_back({min(itR->second, i.second), max(itR->second, i.second)});
 			}
-			else
-				vR.push_back(i);
+			else if(pow((itR->first).first - i.first.first,2) + pow((itR->first).second - i.first.second,2) < d)
+			{
+				ans.clear();
+				d = pow((itR->first).first - i.first.first,2) + pow((itR->first).second - i.first.second,2);
+				ans.push_back({min(itR->second, i.second), max(itR->second, i.second)});
+			}
+			itR++;
 		}
 	}
-	//for(auto i : v)
-	//	cout<<"("<<i.first<<","<<i.second<<")"<<endl;
-	
-	for(auto i : vL)
-		cout<<"vLis : ("<<i.first<<","<<i.second<<")"<<endl;
-	cout<<"----------"<<endl;
-	for(auto i : vR)
-		cout<<"vRis : ("<<i.first<<","<<i.second<<")"<<endl;
-	
-	//ans = _2DCP(v);
+	return d;
+}
+int main(int argc, char const *argv[])
+{
+	int N;
+	vector<pair<pair<int,int>,size_t>> point;
+	cin >> N;
+	point.resize(N);
+	for(int i = 0; i < N; i++)
+	{
+		cin >> point.at(i).first.first >> point.at(i).first.second;
+		point.at(i).second = i+1; 
+	}
+	vector<pair<size_t,size_t>> ans;
+	sort(point.begin(), point.end(), mycmp);
+	long long d = _2DCP(point, ans);
+	//cout<<"------------------"<<endl;
+	cout << d <<" "<< ans.size()<<"\n";
+	sort(ans.begin(), ans.end());
+	for(auto i : ans)
+		cout << i.first <<" "<<i.second<<"\n";
 	return 0;
 }
