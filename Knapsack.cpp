335fb088e588269bb2cf6c1e1722c7865033b000
WilliamChen12340 <31947682+WilliamChen12340@users.noreply.github.com>
2020-03-02 18:43:07 +0800

    second commit

--- Knapsack.cpp
+++ Knapsack.cpp
@@ -3,65 +3,78 @@
 #include<vector>
 using namespace std;
 double lb = 0;
-typedef struct {
+typedef struct{
 	double value;
 	double weight;
-	double cp_value;
+	double cp;
 }item;
-bool mycmp(const item& i1, const item& i2){
-	return i1.cp_value < i2.cp_value;
+bool cmp(const item& i1, const item& i2){
+	return i1.cp > i2.cp;
 }
-double upper_bound(int W, vector<item> vi, double taken){
-	double ub = taken;
-	while(!vi.empty() && W > vi.back().weight){
-		item i = vi.back();
-		ub += i.value;
-		W -= i.weight;
-		vi.pop_back();
-	}
-	if(!vi.empty())
-		ub += W * vi.back().cp_value;
-	return ub;
-}
-void knapsack(int W, vector<item> vi, double taken){
-	item ith = vi.back();
-	vi.pop_back();
-	double ub1 = upper_bound(W - ith.weight, vi, taken + ith.value);
-	double ub0 = upper_bound(W, vi, taken);
-	if(vi.empty())
-		lb = (W >= ith.weight)? max(lb, taken + ith.value) : max(lb, taken);
-	else{
-		if(ub1 >= ub0){
-			if(lb >= ub1)	;
-			else{
-				if(W >= ith.weight)	knapsack(W - ith.weight, vi, taken + ith.value);
-				if(ub0 > lb)	knapsack(W, vi, taken);
-			}
+double upper_bound(double capacity, double taken, vector<item> vi){
+	double ub = 0;
+	auto it = vi.begin();
+	while(capacity > 0)
+	{
+		if(it->weight <= capacity)
+		{
+			capacity -= it->weight;
+			ub += it->value;
+			it++;
 		}
-		else{
-			if(lb >= ub0)	;
-			else{
-				knapsack(W, vi, taken);
-				if(ub1 > lb){
-					if(W >= ith.weight)	knapsack(W - ith.weight, vi, taken + ith.value);
-				}
-			}
+		else
+		{
+			ub += it->cp * capacity;
+			capacity = 0;
 		}
 	}
+	return taken + ub;
 }
-int main(){
-	int W, I;
+void knapsack(double capacity, double taken, vector<item> vi){
+	//initial condition
+	if(vi.empty() || capacity == 0)
+		lb = max(lb, taken);
+	//recursive
+	double ub_taken = 0;
+	vector<item> temp = vi;
+	temp.erase(vi.begin());
+	if(capacity >= vi.begin()->weight)
+		ub_taken = upper_bound(capacity - vi.begin()->weight, taken + vi.begin()->value, temp);
+	double ub_not_taken = upper_bound(capacity, taken, temp);
+	cout<<ub_taken<<"\n";
+	cout<<ub_not_taken<<"\n";
+	//double lb_taken, lb_not_taken;
+	if(ub_taken > ub_not_taken)
+	{
+		if(capacity >= vi.begin()->weight)
+			knapsack(capacity - vi.begin()->weight, taken + vi.begin()->value, temp);
+		if(lb < ub_not_taken)
+			knapsack(capacity, taken, temp);
+	}
+	else
+	{
+		knapsack(capacity, taken, temp);
+		if(lb < ub_taken)
+		{
+			if(capacity >= vi.begin()->weight)
+				knapsack(capacity - vi.begin()->weight, taken + vi.begin()->value, temp);
+		}
+	}
+	return 0;
+}
+int main()
+{
+	double W, I;
 	cin >> W >> I;
 	vector<item> vi;
 	vi.resize(I);
-	for(int i=0; i<I; i++){
-		item temp_i;
-		cin >> temp_i.value >> temp_i.weight;
-		temp_i.cp_value = temp_i.value / temp_i.weight;
-		vi.at(i) = temp_i;
+	for(int i = 0; i < I; i++){
+		cin >> vi.at(i).value >> vi.at(i).weight;
+		vi.at(i).cp = vi.at(i).value / vi.at(i).weight;
 	}
-	sort(vi.begin(), vi.end(), mycmp);
-	knapsack(W, vi, 0);
-	cout<<lb<<"\n";
+	sort(vi.begin(), vi.end(), cmp);
+	knapsack(W, 0, vi);
+	//cout<<upper_bound(W, 0, vi);
+	//for(auto i : vi)	cout<<i.cp<<"\n";
 	return 0;
 }
